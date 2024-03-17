c98cfcbb0ce63f92244552849a85d38f28e825a6
Abid Ali <abdsoftfsd@gmail.com>
2024-03-17 14:23:11 +0500

    Exceptions

--- src/main/kotlin/RuntimeExceptions.kt
+++ src/main/kotlin/RuntimeExceptions.kt
@@ -6,4 +6,26 @@ fun main() {
     } catch (t: Throwable) {
         println(t.message)
     }
+
+    // we can also assign the value from try catch like if else
+    val ans = try {
+        nums[1]
+    } catch (t: Throwable) {
+        0
+    }
+
+    println("The answer is $ans")
+
+    try {
+        throwException()
+    } catch (t: Throwable) {
+        println(t.message)
+    } finally {
+        println("This is the finally block")
+    }
+}
+
+
+fun throwException() {
+    throw Exception("This is an exception")
 }
