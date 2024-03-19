48a0903df88badd6c1287f394db44d46740e3f00
Abid Ali <abdsoftfsd@gmail.com>
2024-03-19 09:58:26 +0500

    039 Destructuring

--- src/main/kotlin/com/rsk/DataClasses.kt
+++ src/main/kotlin/com/rsk/DataClasses.kt
@@ -6,7 +6,12 @@ data class Person(val name: String, val age: Int) {
 
 fun main() {
     val sam = Person("Sam", 31)
-    val samuel = Person("Sam", 32)
+
+    // this will copy the reference
+    // val samuel = sam.copy()
+
+    // this will copy the data
+    val samuel = sam.copy(age = 32)
 
     println("$sam has hashcode ${sam.hashCode()}")
     println("$samuel has hashcode ${samuel.hashCode()}")
@@ -16,4 +21,11 @@ fun main() {
     } else {
         println("Sam and Samuel are not equal")
     }
+
+    // Without Destructuring
+    println("Samuel has the ${samuel.name} and the age ${samuel.age}")
+
+    // With Destructuring
+    val (name, age) = samuel
+    println("Samuel has the $name and the age $age")
 }
